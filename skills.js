5ac8a7cbe3938d8f84cf26d9f5b28340d284bdd7
Md. Nahid Hasan <nahid1719abd@gmail.com>
2026-06-07 23:14:05 +0600

    added skills page

---
+++ skills.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const cards = document.querySelectorAll(".skill-card");
+
+    cards.forEach((card, index) => {
+
+        card.style.opacity = "0";
+        card.style.transform = "translateY(40px)";
+
+        setTimeout(() => {
+            card.style.transition = "all .6s ease";
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+        }, index * 120);
+
+    });
+
+});
